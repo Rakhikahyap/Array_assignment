c8ac212bdaf9f25c972c0de7ca459481a45b43e7
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-11-14 11:51:18 +0530

    Add files via upload

---
+++ array_asing.py
@@ -0,0 +1,10 @@
+# a= [2,3,4,5,6,7,8]
+# b=a[::-1]
+# print('rev list: ',b)
+
+# list =[2,4,1,5,6,7,8,9,1]
+# list2=[]
+# print('before rev:',list)
+# for i in range(len(list)-1,-1,-1):
+#         list2.append(list[i])
+# print('after rev:',list2)
